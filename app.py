f088e952b8c09b29d2ed694e0869f1a13135c562
Zanillect58 <andres.galeano@usantoto.edu.co>
2025-05-22 14:13:31 -0500

    CAMBIOS EN NAVBAR Y FOOTER

--- app.py
+++ app.py
@@ -14,14 +14,6 @@ from flask import send_from_directory
 
 from flask import send_file
 from io import BytesIO
-from openpyxl import Workbook
-
-from reportlab.lib.pagesizes import A4
-from reportlab.pdfgen import canvas
-from reportlab.lib.units import cm
-from reportlab.lib import colors
-
-from reportlab.lib.utils import ImageReader
 
 import os
 
